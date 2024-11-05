cf95ea0dde747982a76600eeee115230e6257f6a
Surya Sharma <surya.sharma233@gmail.com>
2024-11-05 16:53:53 -0600

    https support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod event;
 mod attendee;
 
+use axum::handler::HandlerWithoutStateExt;
 use attendee::attendee::{create_attendee, get_attendees_by_event_id, Attendee};
 use axum::{
+    http::{StatusCode, Uri},
     routing::{get, post},
     Router,
-    extract::{State, Form, Path},
-    response::{Html, Redirect}
+    extract::{State, Form, Path, Host},
+    response::{Html, Redirect},
+    BoxError
 };
+use axum_server::tls_rustls::RustlsConfig;
 use std::sync::Arc;
 use r2d2_sqlite::SqliteConnectionManager;
 use minijinja::{Environment, context};
@@ -17,6 +21,7 @@ use chrono::prelude::*;
 use uuid::Uuid;
 use event::event::{get_event_by_id, Event};
 use event::event::create_event;
+use std::net::SocketAddr;
 
 #[derive(Deserialize)]
 #[derive(Debug)]
@@ -40,6 +45,12 @@ struct NewAttendeeForm {
     last_name: String
 }
 
+#[derive(Clone, Copy)]
+struct Ports {
+    http: u16,
+    https: u16
+}
+
 #[tokio::main]
 async fn main() {
     let manager = SqliteConnectionManager::file("./join.db");
@@ -58,10 +69,18 @@ async fn main() {
         .route("/new_attendee/:event_id", post(new_attendee_post))
         .with_state(shared_state);
 
-    //should panic
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    //should panic
-    axum::serve(listener, app).await.unwrap();
+    let ports = Ports {
+        http: 3000,
+        https: 7878
+    };
+
+    tokio::spawn(redirect_http_to_https(ports));
+    let config = RustlsConfig::from_pem_file("cert.pem", "key.pem").await.unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], ports.https));
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
 fn make_env() -> Environment<'static> {
@@ -178,4 +197,38 @@ async fn new_attendee_post(State(state): State<Arc<join::AppState>>, Path(event_
 
     let redir_url = format!("/event/{}", event_id);
     Redirect::to(&redir_url)
+}
+
+//pretty much lifted directly from: https://github.com/tokio-rs/axum/blob/main/examples/tls-rustls/src/main.rs
+//maybe revisit at some point
+async fn redirect_http_to_https(ports: Ports) {
+    fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri, BoxError> {
+        let mut parts = uri.into_parts();
+
+        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+
+        if parts.path_and_query.is_none() {
+            parts.path_and_query = Some("/".parse().unwrap());
+        }
+
+        let https_host = host.replace(&ports.http.to_string(), &ports.https.to_string());
+        parts.authority = Some(https_host.parse()?);
+
+        Ok(Uri::from_parts(parts)?)
+    }
+
+    let redirect = move |Host(host): Host, uri: Uri| async move {
+        match make_https(host, uri, ports) {
+            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+            Err(_error) => {
+                Err(StatusCode::BAD_REQUEST)
+            }
+        }
+    };
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], ports.http));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, redirect.into_make_service())
+        .await
+        .unwrap();
 }
